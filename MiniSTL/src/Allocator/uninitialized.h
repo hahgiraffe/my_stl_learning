cead0496ab7592d6d7dab821a104627f0be5e818
hahgiraffe <646268684@qq.com>
2019-11-22 17:36:19 +0800

    add uninitialized

--- MiniSTL/src/Allocator/uninitialized.h
+++ MiniSTL/src/Allocator/uninitialized.h
@@ -5,11 +5,100 @@
  */
 #ifndef MINISTL_SRC_ALLOCATOR_UNINITIALIZED
 #define MINISTL_SRC_ALLOCATOR_UNINITIALIZED
+#include "./construct.h"
+#include "../Iterator/typetraits.h"
+#include "../Iterator/stl_iterator.h"
 
 namespace MINISTL{
 
+//uninitialized_copy,将[first,last)移动到[res,res+last-first)
+template< typename InputIterator, typename ForwardIterator>
+ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator res){
+    return __uninitialized_copy(first, last, res, value_type(first));
+}
+
+template< typename InputIterator, typename ForwardIterator,typename T>
+ForwardIterator uninitialized_copy(InputIterator first, InputIterator last, ForwardIterator res, T*){
+    typedef typename _type_traits<T>::is_POD_type is_POD;
+    return __uninitialized_copy_aux(first, last, res, is_POD());
+}
+
+template< typename InputIterator, typename ForwardIterator>
+ForwardIterator uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator res, __true_type){
+    return copy(first, last, res);
+}
+
+template< typename InputIterator, typename ForwardIterator>
+ForwardIterator uninitialized_copy_aux(InputIterator first, InputIterator last, ForwardIterator res, __false_type){
+    ForwardIterator cur;
+    for(; first != last; ++first, ++cur){
+        construct(&*cur, *first);
+    }
+    return cur;
+}
+
+//针对char*、wchar_t*存在特化高效版本 memmove直接移动内存
+char *uninitialized_copy(const char *first, const char *last, char *result) {
+    memmove(result, first, last - first);
+    return result + (last - first);
+}
+
+wchar_t *uninitialized_copy(const wchar_t *first, const wchar_t *last, wchar_t *result) {
+    memmove(result, first, sizeof(wchar_t) * (last - first));
+    return result + (last - first);
+}
+
+//uninitialized_fill，将[first,last)填充value
+template< typename ForwardIterator, typename T>
+void uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& value){
+    __uninitialized_fill(first, last, x, value_type(first));
+}
+
+template< typename ForwardIterator, typename T, typename T1>
+void __uninitialized_fill(ForwardIterator first, ForwardIterator last, const T& value, T1*){
+    typedef typename _type_traits<T1>::is_POD_type is_POD;
+    __uninitialized_fill__aux(first, last, x, is_POD());
+}
+
+template< typename ForwardIterator, typename T>
+void __uninitialized_fill__aux(ForwardIterator first, ForwardIterator last, const T& value, __true_type){
+    fill(first, last, x);
+}
+
+template< typename ForwardIterator, typename T>
+void __uninitialized_fill__aux(ForwardIterator first, ForwardIterator last, const T& value, __false_type){
+    ForwardIterator cur = first;
+    for(; cur != last; ++cur){
+        construct(&*cur, value);
+    }
+}
 
 
+//uninitialized_fill_n，将[first,first+n)填充value
+template< typename ForwardIterator,typename Size, typename T>
+ForwardIterator uninitialized_fill_n(ForwardIterator first, Size n, const T& value){
+    return __uninitialized_fill_n(first, n, value, value_type(first));
+}
+
+template< typename ForwardIterator,typename Size, typename T, typename T1>
+ForwardIterator __uninitialized_fill_n(ForwardIterator first, Size n, const T& value,T1*){
+    typedef typename _type_traits<T1>::is_POD_type is_POD;
+    return __uninitialized_fill_n_aux(first, n, value, is_POD());
+}
+
+template< typename ForwardIterator,typename Size, typename T>
+ForwardIterator __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& value, __true_type){
+    return fill_n(first, n, value);
+}
+
+template< typename ForwardIterator,typename Size, typename T>
+ForwardIterator __uninitialized_fill_n_aux(ForwardIterator first, Size n, const T& value, __false_type){
+    ForwardIterator cur = first;
+    for(;n>0;--n,++cur){
+        construct(&*cur,value);
+    }
+    return cur;
+}
 
 }
 
--- MiniSTL/src/Iterator/stl_iterator.h
+++ MiniSTL/src/Iterator/stl_iterator.h
@@ -5,9 +5,120 @@
  */
 #ifndef MINISTL_SRC_ALLOCATOR_ITERATOR_STL_ITERATOR_H
 #define MINISTL_SRC_ALLOCATOR_ITERATOR_STL_ITERATOR_H
+#include <stddef.h>
 
 namespace MINISTL{
 
+    //iterator类型
+    struct input_iterator_tag { };
+    struct output_iterator_tag { };
+    struct forward_iterator_tag : public input_iterator_tag { };
+    struct bidirectional_iterator_tag : public forward_iterator_tag { };
+    struct random_access_iterator_tag : public bidirectional_iterator_tag { };
+
+
+    template<typename Category, typename T, typename Distance = ptrdiff_t, typename Pointer = T*, typename Reference = T*>
+    struct iterator
+    {
+        typedef Category iterator_category; //迭代器类型
+        typedef T value_type;               //迭代器指向值类型
+        typedef Distance difference_type;   //距离
+        typedef Pointer pointer;            //指针
+        typedef Reference reference;        //引用
+    };
+
+    //迭代器萃取剂
+    template <typename Iterator>
+    struct iterator_traits {
+        typedef typename Iterator::iterator_category iterator_category;
+        typedef typename Iterator::value_type value_type;
+        typedef typename Iterator::difference_type difference_type;
+        typedef typename Iterator::pointer pointer;
+        typedef typename Iterator::reference reference;
+    };
+
+    //偏特化
+    template <typename T>
+    struct iterator_traits<T*>
+    {
+        typedef random_access_iterator_tag iterator_category;
+        typedef T value_type;
+        typedef ptrdiff_t difference_type;
+        typedef T* pointer;
+        typedef T& reference;   
+    };
+    
+    template <typename T>
+    struct iterator_traits<const T*>
+    {
+        typedef random_access_iterator_tag iterator_category;
+        typedef T value_type;
+        typedef ptrdiff_t difference_type;
+        typedef const T* pointer;
+        typedef const T& reference;   
+    };
+    
+    //萃取出迭代器类型
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::iterator_category iterator_category(const Iterator&){
+        typedef typename iterator_traits<Iterator>::iterator_category category;
+        return category();
+    }
+
+    //萃取出两个迭代器之间的距离distance type
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::difference_type* difference_type(const Iterator&){
+        return static_cast<typename iterator_traits<Iterator>::difference_type*>(0);
+    }
+
+    //萃取出迭代器所指对象value type
+    template <typename Iterator>
+    inline typename iterator_traits<Iterator>::value_type* value_type(const Iterator&){
+        return static_cast<typename iterator_traits<Iterator>::value_type*>(0);
+    }
+
+    //以下为整组distance函数
+    template <class InputIterator>
+    inline typename iterator_traits<InputIterator>::diffrence_type __distance(InputIterator first, InputIterator last, input_iterator_tag) {
+        iterator_traits<InputIterator>::diffrence_type n = 0;
+        while (first != last)
+            ++first, ++n;
+        return n;
+    }
+
+    template <class InputIterator>
+    inline typename iterator_traits<InputIterator>::diffrence_type __distance(InputIterator first, InputIterator last, random_access_iterator_tag) {
+        return last - first;
+    }
+
+    template <class InputIterator>
+    inline typename iterator_traits<InputIterator>::diffrence_type distance(InputIterator first, InputIterator last) {
+        typename iterator_traits<InputIterator>::iterator_category category;
+        return __distance(first, last, category());
+    }
+
+    //以下为整组advance函数
+    template <class InputIterator, class Distance>
+    inline void __advance(InputIterator &i, Distance n, input_iterator_tag) {
+        while (n--) ++i;
+    }
+
+    template <class InputIterator, class Distance>
+    inline void __advance(InputIterator &i, Distance n, bidirectional_iterator_tag) {
+        if (n >= 0)
+            while (n--) ++i;
+        else
+            while (n++) --i;
+    }
+
+    template <class InputIterator, class Distance>
+    inline void __advance(InputIterator &i, Distance n, random_access_iterator_tag) {
+        i += n;
+    }
+
+    template <class InputIterator, class Distance> inline void advance(InputIterator &i, Distance n) {
+        __advance(i, n, iterator_traits<InputIterator>::iterator_category());
+    }
 
 
 }   //MINISTL
